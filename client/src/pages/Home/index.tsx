bcf9f76ad1c423d64265433d93937d9f4b1f2e1a
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-28 11:01:44 +0300

    add show btns

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -6,6 +6,12 @@ import './_home.scss';
 const Home = () => {
   return (
     <main className='home'>
+      <div className='home__show'>
+        <button className='btn home__show-books' disabled>
+          Books
+        </button>
+        <button className='btn home__show-authors'>Authors</button>
+      </div>
       <SearchForm />
     </main>
   );
