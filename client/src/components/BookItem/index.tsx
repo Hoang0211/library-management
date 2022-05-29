67ae3602e279b042d19873c1cb74c639a181bb83
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-29 17:56:03 +0300

    add book book items layout

---
+++ client/src/components/BookItem/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { RiBookFill } from 'react-icons/ri';
+
+import { Book } from '../../types';
+import './_bookItem.scss';
+
+export type BookItemProps = {
+  book: Book;
+  lastItem: boolean;
+};
+
+const BookItem = ({ book, lastItem }: BookItemProps) => {
+  return (
+    <div className={`book-item ${lastItem && 'book-item-last'}`}>
+      <div className='info'>
+        <p className='title'>
+          {book.title} ({book.publishedDate.getFullYear()})
+        </p>
+        <div className='category'>
+          <RiBookFill className='icon category__icon' />
+          <span className='category__text'>{book.category}</span>
+        </div>
+        <p className='authors'>Authors: {book.authors.join(', ')}</p>
+        <p className='status'>Status: Available</p>
+      </div>
+      <button className='btn btn-details'>More Details</button>
+    </div>
+  );
+};
+
+export default BookItem;
---
+++ client/src/types.ts
@@ -0,0 +1,32 @@
+export enum Category {
+  Journal = 'journal',
+  Article = 'article',
+  Book = 'book',
+  Thesis = 'thesis',
+  Other = 'other',
+}
+
+export enum Status {
+  Available = 'available',
+  Borrowed = 'borrowed',
+}
+
+export type Book = {
+  _id: string;
+  isbn: string;
+  title: string;
+  description: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: Date;
+  category: string;
+  numPage: number;
+  status: Status;
+};
+
+export type ApiDataType = {
+  message: string;
+  status: string;
+  books: Book[];
+  book?: Book;
+};
