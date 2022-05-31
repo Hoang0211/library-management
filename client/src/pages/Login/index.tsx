fe8d8f84bdd728c108b3e102b47d94eec2d1e5ff
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-31 18:12:13 +0300

    add log in page (sign in with google only)

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
-import SignIn from '../../components/SignForm/SignIn';
-import SignUp from '../../components/SignForm/SignUp';
 import './_login.scss';
 
 const Login = () => {
+  const handleSucess = () => {};
+
+  const clientId =
+    '305036799412-m3i8638rrtmqqa4pu90k9167evp9sndo.apps.googleusercontent.com';
+
   return (
     <main className='login'>
-      <SignIn />
-      {/* <SignUp /> */}
+      <div className='login__container'>
+        <p className='title'>Log In</p>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin onSuccess={handleSucess} />
+        </GoogleOAuthProvider>
+      </div>
     </main>
   );
 };
