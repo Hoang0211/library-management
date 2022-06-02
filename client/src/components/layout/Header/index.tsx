322e81f2a2ef5672640ed86179f1c37c1a296dcb
Nguyen Hoang <n3verland0211@gmail.com>
2022-06-02 22:58:40 +0300

    add navigation between pages from header

--- client/src/components/layout/Header/index.tsx
+++ client/src/components/layout/Header/index.tsx
@@ -1,41 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUser, FaBook } from 'react-icons/fa';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { signIn, signOut } from '../../../redux/actions';
-import { AppState, User } from '../../../types';
+import { signOut } from '../../../redux/actions';
+import { AppState, Role } from '../../../types';
 import './_header.scss';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const { user, token } = useSelector((state: AppState) => state.user);
+  const { user } = useSelector((state: AppState) => state.user);
 
-  const logOutTesting = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const toggleOptions = () => {
+    setShowOptions((prevState) => !prevState);
+  };
+
+  const logout = () => {
     dispatch(signOut());
   };
 
   return (
     <header className='header'>
       <nav className='nav'>
-        <Link to='/' className='btn btn-logo'>
-          <FaBook className='icon icon-book' />
+        <Link to='/' className='logo'>
+          <FaBook className='icon logo__icon' />
           <h1 className='logo__text'>Local Library</h1>
         </Link>
-        <button onClick={logOutTesting}>Logout</button>
         {user ? (
-          <button className='btn user'>
-            <FaUser className='icon icon-user' />
+          <div className='user' onClick={toggleOptions}>
+            <FaUser className='icon user__icon user__icon-user' />
             <span className='user__text'>
               {user.firstName + ' ' + user.lastName}
             </span>
-            <MdOutlineKeyboardArrowDown className='icon icon-arrow' />
-          </button>
+            <MdOutlineKeyboardArrowDown className='icon user__icon user__icon-arrow' />
+            <div
+              className={`user__options ${showOptions && `user__options-show`}`}
+            >
+              <Link to='/profile' className='user__option'>
+                Profile
+              </Link>
+
+              {user.role === Role.Admin && (
+                <>
+                  <Link to='/users' className='user__option'>
+                    Users
+                  </Link>
+                  <Link to='/borrows' className='user__option'>
+                    Borrows
+                  </Link>
+                </>
+              )}
+              <Link
+                to='/'
+                className='user__option user__option-last'
+                onClick={logout}
+              >
+                Logout
+              </Link>
+            </div>
+          </div>
         ) : (
-          <Link to='/login' className='btn btn-login'>
-            <FaUser className='icon icon-user' />
-            <span className='btn-login__text'>Login</span>
+          <Link to='/login' className='login-btn'>
+            <FaUser className='icon login-btn__icon' />
+            <span className='login-btn__text'>Login</span>
           </Link>
         )}
       </nav>
