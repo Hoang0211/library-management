9e68475b0727f2b04dc84a688885841c1bad0dc9
Nguyen Hoang <n3verland0211@gmail.com>
2022-06-06 10:54:13 +0300

    add addAuthor page layout

--- client/src/pages/AddAuthor/index.tsx
+++ client/src/pages/AddAuthor/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './_addAuthor.scss';
 
 const AddAuthor = () => {
-  return <main className='add-author'>AddAuthor</main>;
+  const navigate = useNavigate();
+
+  const add = () => {
+    navigate('/');
+  };
+
+  const discard = () => {
+    navigate('/');
+  };
+
+  return (
+    <main className='add-author'>
+      <form className='container'>
+        <h2 className='title'>New Author</h2>
+        <div className='input-controller'>
+          <label>First name</label>
+          <input type='text' placeholder='Enter first name...'></input>
+        </div>
+        <div className='input-controller'>
+          <label>Last name</label>
+          <input type='text' placeholder='Enter last name...'></input>
+        </div>
+        <div className='input-controller'>
+          <label>Biography</label>
+          <textarea placeholder='Enter biography...'></textarea>
+        </div>
+        <div className='btns'>
+          <button className='btn btn-save' type='submit' onClick={add}>
+            Add
+          </button>
+          <button className='btn btn-discard' onClick={discard}>
+            Discard
+          </button>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default AddAuthor;
