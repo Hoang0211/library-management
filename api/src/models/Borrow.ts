07a8de9b440f03b2b83acf4754b72dc2385980b4
Nguyen Hoang <n3verland0211@gmail.com>
2022-06-10 10:35:25 +0300

    update borrow model

--- api/src/models/Borrow.ts
+++ api/src/models/Borrow.ts
@@ -1,30 +1,32 @@
 import mongoose, { Document, Schema, Types } from 'mongoose'
 
 export type BorrowDocument = Document & {
-  bookId: Types.ObjectId
   userId: Types.ObjectId
-  borrowDate: Date
-  returnDate: Date
+  bookIds: Types.ObjectId[]
+  loanDate: Date
+  dueDate: Date
 }
 
 const borrowSchema = new mongoose.Schema({
-  bookId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Book',
-    required: true,
-  },
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
-  borrowDate: {
+  bookIds: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true,
+    },
+  ],
+  loanDate: {
     type: Date,
-    default: Date.now,
+    require: true,
   },
-  returnDate: {
+  dueDate: {
     type: Date,
-    default: () => Date.now() + 7 * 24 * 60 * 60 * 1000 * 2,
+    require: true,
   },
 })
 
