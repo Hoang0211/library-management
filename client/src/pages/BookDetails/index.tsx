8677c2759386b677bdd70b5db69d7be75d66c57c
Nguyen Hoang <n3verland0211@gmail.com>
2022-06-14 12:33:16 +0300

    edit bookDetails layout

--- client/src/pages/BookDetails/index.tsx
+++ client/src/pages/BookDetails/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { MdEdit, MdDelete } from 'react-icons/md';
 
+import PageWrapper from '../../components/layout/PageWrapper';
 import {
   clearGetBookDetailsError,
   deleteBook,
@@ -26,18 +27,18 @@ const BookDetails = () => {
 
   const { bookId } = useParams<{ bookId?: string }>();
 
+  const navigateToEditPage = () => {
+    navigate(`/books/edit/${bookId}`);
+  };
+
+  const navigateToHomePage = () => {
+    navigate('/');
+  };
+
   const deleteBookHandler = () => {
     if (bookId) {
       dispatch(deleteBook(token, bookId));
     }
-  };
-
-  const navigateToEditHandler = () => {
-    navigate(`/books/edit/${bookId}`);
-  };
-
-  const navigateToHomeHandler = () => {
-    navigate('/');
   };
 
   useEffect(() => {
@@ -59,63 +60,131 @@ const BookDetails = () => {
   }, [dispatch, navigate, error, deleteError, deleted]);
 
   return (
-    <main className='book-details'>
-      <div className='container'>
-        <div className='header'>
-          <h2 className='title'>Book Details</h2>
-          {user?.role === Role.Admin && (
-            <div className='btns'>
-              <button className='btn btn-edit' onClick={navigateToEditHandler}>
-                <MdEdit />
-              </button>
-              <button className='btn btn-delete' onClick={deleteBookHandler}>
-                <MdDelete />
-              </button>
-            </div>
-          )}
+    <PageWrapper className='book-details'>
+      <div className='title'>
+        <h1>Book Details</h1>
+        <div className='actions'>
+          <button className='action' onClick={navigateToEditPage}>
+            Edit
+          </button>
+          <button className='action' onClick={deleteBookHandler}>
+            Delete
+          </button>
+          <button className='action' onClick={navigateToHomePage}>
+            Home
+          </button>
         </div>
-        <p className='isbn'>ISBN: {!loading && book && book.isbn}</p>
-        <p className='book-title'>Title: {!loading && book && book.title}</p>
-        <p className='description'>
-          Description: {!loading && book && book.description}
-        </p>
-        <p className='authors'>
-          Authors:{' '}
-          {!loading &&
-            book &&
-            book.authors
-              .map((author) => author.firstName + ' ' + author.lastName)
-              .join(', ')}
-        </p>
-        <p className='publisher'>
-          Publisher: {!loading && book && book.publisher}
-        </p>
-        <p className='published-date'>
-          Published date:{' '}
-          {!loading &&
-            book &&
-            new Date(book.publishedDate).toLocaleDateString()}
-        </p>
-        <p className='category'>
-          Category:{' '}
-          {!loading &&
-            book &&
-            book.category.charAt(0).toUpperCase() + book.category.slice(1)}
-        </p>
-        <p className='num-page'>
-          Number of pages: {!loading && book && book.numPage}
-        </p>
-        <p className='status'>
-          Status:{' '}
-          {!loading &&
-            book &&
-            book.status.charAt(0).toUpperCase() + book.status.slice(1)}
-        </p>
-        <button className='btn btn-home' onClick={navigateToHomeHandler}>
-          Home
-        </button>
       </div>
-    </main>
+      <div className='container'>
+        <h2 className='book-details__title'>
+          {!loading && book && book.title}
+        </h2>
+        <div className='book-details__info'>
+          <div className='book-details__info-left'>
+            {' '}
+            <p className='book-details__isbn'>
+              ISBN: {!loading && book && book.isbn}
+            </p>
+            <p className='book-details__description'>
+              Description: {!loading && book && book.description}
+            </p>
+            <p className='book-details__authors'>
+              Authors:{' '}
+              {!loading &&
+                book &&
+                book.authors
+                  .map((author) => author.firstName + ' ' + author.lastName)
+                  .join(', ')}
+            </p>
+            <p className='book-details__category'>
+              Category:{' '}
+              {!loading &&
+                book &&
+                book.category.charAt(0).toUpperCase() + book.category.slice(1)}
+            </p>
+          </div>
+          <div className='book-details__info-right'>
+            {' '}
+            <p className='book-details__publisher'>
+              Publisher: {!loading && book && book.publisher}
+            </p>
+            <p className='book-details__published-date'>
+              Published date:{' '}
+              {!loading &&
+                book &&
+                new Date(book.publishedDate).toLocaleDateString()}
+            </p>
+            <p className='book-details__num-page'>
+              Number of pages: {!loading && book && book.numPage}
+            </p>
+            <p className='book-details__status'>
+              Status:{' '}
+              {!loading &&
+                book &&
+                book.status.charAt(0).toUpperCase() + book.status.slice(1)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </PageWrapper>
+
+    // <main className='book-details'>
+    //   <div className='book-details__container'>
+    //     <div className='header'>
+    //       <h2 className='title'>Book Details</h2>
+    //       {user?.role === Role.Admin && (
+    //         <div className='btns'>
+    //           <button className='btn btn-edit' onClick={navigateToEditHandler}>
+    //             <MdEdit />
+    //           </button>
+    //           <button className='btn btn-delete' onClick={deleteBookHandler}>
+    //             <MdDelete />
+    //           </button>
+    //         </div>
+    //       )}
+    //     </div>
+    //     <p className='isbn'>ISBN: {!loading && book && book.isbn}</p>
+    //     <p className='book-title'>Title: {!loading && book && book.title}</p>
+    //     <p className='description'>
+    //       Description: {!loading && book && book.description}
+    //     </p>
+    //     <p className='authors'>
+    //       Authors:{' '}
+    //       {!loading &&
+    //         book &&
+    //         book.authors
+    //           .map((author) => author.firstName + ' ' + author.lastName)
+    //           .join(', ')}
+    //     </p>
+    //     <p className='publisher'>
+    //       Publisher: {!loading && book && book.publisher}
+    //     </p>
+    //     <p className='published-date'>
+    //       Published date:{' '}
+    //       {!loading &&
+    //         book &&
+    //         new Date(book.publishedDate).toLocaleDateString()}
+    //     </p>
+    //     <p className='category'>
+    //       Category:{' '}
+    //       {!loading &&
+    //         book &&
+    //         book.category.charAt(0).toUpperCase() + book.category.slice(1)}
+    //     </p>
+    //     <p className='num-page'>
+    //       Number of pages: {!loading && book && book.numPage}
+    //     </p>
+    //     <p className='status'>
+    //       Status:{' '}
+    //       {!loading &&
+    //         book &&
+    //         book.status.charAt(0).toUpperCase() + book.status.slice(1)}
+    //     </p>
+    //     <button className='btn btn-home' onClick={navigateToHomeHandler}>
+    //       Home
+    //     </button>
+    //   </div>
+    // </main>
   );
 };
 
