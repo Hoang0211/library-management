669f0b6e25a9769e79de9a04460224a09ac266e6
Nguyen Hoang <n3verland0211@gmail.com>
2022-06-14 13:21:38 +0300

    edit authorDetails layout

--- client/src/pages/AuthorDetails/index.tsx
+++ client/src/pages/AuthorDetails/index.tsx
@@ -11,10 +11,12 @@ import {
 } from '../../redux/actions';
 import { AppState, Role } from '../../types';
 import './_authorDetails.scss';
+import PageWrapper from '../../components/layout/PageWrapper';
 
 const AuthorDetails = () => {
   const dispatch = useDispatch<any>();
   const navigate = useNavigate();
+  const { authorId } = useParams<{ authorId?: string }>();
 
   const { user, token } = useSelector((state: AppState) => state.user);
   const { loading, error, author } = useSelector(
@@ -24,20 +26,18 @@ const AuthorDetails = () => {
     (state: AppState) => state.deleteAuthor
   );
 
-  const { authorId } = useParams<{ authorId?: string }>();
+  const navigateToEditPage = () => {
+    navigate(`/authors/edit/${authorId}`);
+  };
+
+  const navigateToHomePage = () => {
+    navigate('/');
+  };
 
   const deleteAuthorHandler = () => {
     if (authorId) {
       dispatch(deleteAuthor(token, authorId));
     }
-  };
-
-  const navigateToEditHandler = () => {
-    navigate(`/authors/edit/${authorId}`);
-  };
-
-  const navigateToHomeHandler = () => {
-    navigate('/');
   };
 
   useEffect(() => {
@@ -59,21 +59,26 @@ const AuthorDetails = () => {
   }, [dispatch, navigate, error, deleteError, deleted]);
 
   return (
-    <main className='author-details'>
-      <div className='container'>
-        <div className='header'>
-          <h2 className='title'>Author Details</h2>
+    <PageWrapper className='author-details'>
+      <div className='title'>
+        <h1>Book Details</h1>
+        <div className='actions'>
           {user?.role === Role.Admin && (
-            <div className='btns'>
-              <button className='btn btn-edit' onClick={navigateToEditHandler}>
-                <MdEdit />
+            <>
+              <button className='action' onClick={navigateToEditPage}>
+                Edit
               </button>
-              <button className='btn btn-delete' onClick={deleteAuthorHandler}>
-                <MdDelete />
+              <button className='action' onClick={deleteAuthorHandler}>
+                Delete
               </button>
-            </div>
+            </>
           )}
+          <button className='action' onClick={navigateToHomePage}>
+            Home
+          </button>
         </div>
+      </div>
+      <div className='container'>
         <p className='first-name'>
           First name: {!loading && author && author.firstName}
         </p>
@@ -89,11 +94,43 @@ const AuthorDetails = () => {
             author &&
             author.books.map((book) => <li key={book._id}>{book.title}</li>)}
         </ul>
-        <button className='btn btn-home' onClick={navigateToHomeHandler}>
-          Home
-        </button>
       </div>
-    </main>
+    </PageWrapper>
+    // <main className='author-details'>
+    //   <div className='container'>
+    //     <div className='header'>
+    //       <h2 className='title'>Author Details</h2>
+    //       {user?.role === Role.Admin && (
+    //         <div className='btns'>
+    //           <button className='btn btn-edit' onClick={navigateToEditHandler}>
+    //             <MdEdit />
+    //           </button>
+    //           <button className='btn btn-delete' onClick={deleteAuthorHandler}>
+    //             <MdDelete />
+    //           </button>
+    //         </div>
+    //       )}
+    //     </div>
+    //     <p className='first-name'>
+    //       First name: {!loading && author && author.firstName}
+    //     </p>
+    //     <p className='last-name'>
+    //       Last name: {!loading && author && author.lastName}
+    //     </p>
+    //     <p className='biography'>
+    //       Biography: {!loading && author && author.biography}
+    //     </p>
+    //     <p className='book-list'>List of book:</p>
+    //     <ul>
+    //       {!loading &&
+    //         author &&
+    //         author.books.map((book) => <li key={book._id}>{book.title}</li>)}
+    //     </ul>
+    //     <button className='btn btn-home' onClick={navigateToHomeHandler}>
+    //       Home
+    //     </button>
+    //   </div>
+    // </main>
   );
 };
 
--- client/src/pages/BookDetails/index.tsx
+++ client/src/pages/BookDetails/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
-import { MdEdit, MdDelete } from 'react-icons/md';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import PageWrapper from '../../components/layout/PageWrapper';
 import {
@@ -16,6 +15,7 @@ import './_bookDetails.scss';
 const BookDetails = () => {
   const dispatch = useDispatch<any>();
   const navigate = useNavigate();
+  const { bookId } = useParams<{ bookId?: string }>();
 
   const { user, token } = useSelector((state: AppState) => state.user);
   const { loading, error, book } = useSelector(
@@ -24,8 +24,6 @@ const BookDetails = () => {
   const { error: deleteError, deleted } = useSelector(
     (state: AppState) => state.deleteBook
   );
-
-  const { bookId } = useParams<{ bookId?: string }>();
 
   const navigateToEditPage = () => {
     navigate(`/books/edit/${bookId}`);
@@ -64,12 +62,17 @@ const BookDetails = () => {
       <div className='title'>
         <h1>Book Details</h1>
         <div className='actions'>
-          <button className='action' onClick={navigateToEditPage}>
-            Edit
-          </button>
-          <button className='action' onClick={deleteBookHandler}>
-            Delete
-          </button>
+          {user?.role === Role.Admin && (
+            <>
+              <button className='action' onClick={navigateToEditPage}>
+                Edit
+              </button>
+              <button className='action' onClick={deleteBookHandler}>
+                Delete
+              </button>
+            </>
+          )}
+
           <button className='action' onClick={navigateToHomePage}>
             Home
           </button>
@@ -127,64 +130,6 @@ const BookDetails = () => {
         </div>
       </div>
     </PageWrapper>
-
-    // <main className='book-details'>
-    //   <div className='book-details__container'>
-    //     <div className='header'>
-    //       <h2 className='title'>Book Details</h2>
-    //       {user?.role === Role.Admin && (
-    //         <div className='btns'>
-    //           <button className='btn btn-edit' onClick={navigateToEditHandler}>
-    //             <MdEdit />
-    //           </button>
-    //           <button className='btn btn-delete' onClick={deleteBookHandler}>
-    //             <MdDelete />
-    //           </button>
-    //         </div>
-    //       )}
-    //     </div>
-    //     <p className='isbn'>ISBN: {!loading && book && book.isbn}</p>
-    //     <p className='book-title'>Title: {!loading && book && book.title}</p>
-    //     <p className='description'>
-    //       Description: {!loading && book && book.description}
-    //     </p>
-    //     <p className='authors'>
-    //       Authors:{' '}
-    //       {!loading &&
-    //         book &&
-    //         book.authors
-    //           .map((author) => author.firstName + ' ' + author.lastName)
-    //           .join(', ')}
-    //     </p>
-    //     <p className='publisher'>
-    //       Publisher: {!loading && book && book.publisher}
-    //     </p>
-    //     <p className='published-date'>
-    //       Published date:{' '}
-    //       {!loading &&
-    //         book &&
-    //         new Date(book.publishedDate).toLocaleDateString()}
-    //     </p>
-    //     <p className='category'>
-    //       Category:{' '}
-    //       {!loading &&
-    //         book &&
-    //         book.category.charAt(0).toUpperCase() + book.category.slice(1)}
-    //     </p>
-    //     <p className='num-page'>
-    //       Number of pages: {!loading && book && book.numPage}
-    //     </p>
-    //     <p className='status'>
-    //       Status:{' '}
-    //       {!loading &&
-    //         book &&
-    //         book.status.charAt(0).toUpperCase() + book.status.slice(1)}
-    //     </p>
-    //     <button className='btn btn-home' onClick={navigateToHomeHandler}>
-    //       Home
-    //     </button>
-    //   </div>
-    // </main>
   );
 };
 
